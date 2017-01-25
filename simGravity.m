e87d731707f23b8d5c7d95d6840a31a09c96112c
Killian Ancel <killian.a@live.ca>
2017-01-25 16:53:18 -0500

    Update simGravity.m

--- simGravity.m
+++ simGravity.m
@@ -1,25 +1,24 @@
 function simGravity
-    n = 3; 
+    n = 2; 
     obj = zeros(n,7);
-    obj(1,:) = [1.5,1,1,0,-0.6,0,0.8];
-    obj(2,:) = [1,-1,0,0,0.6,-0.7,0];
-    obj(3,:) = [2,0,1,0,0.5,0,0.5];
+    obj(1,:) = [3,1,0,0,0,0,-0.5];
+    obj(2,:) = [3,-1,0,0,0,0,0.5];
     color = zeros(n,3);
     for i = 1:n
         color(i,:) = abs(sin(obj(i,1)/max(obj(:,1)) + [0 pi/3 2*pi/3]));
     end
-    T = 500; dt = 0.01;
+    T = 620; dt = 0.01;
     for j = 1:T
         obj = calcMotion(obj,n,dt);
         scatter3(obj(:,2),obj(:,3),obj(:,4),10,color,'filled');
         whitebg('k');
         set(gca,'XTickLabel',[],'YTickLabel',[],'ZTickLabel',[]);
         grid on; grid minor;
-        axis([-5 5 -5 5 -5 5]);
+        axis([-1 1 -1 1 -1 1]);
         pause(0.001);
     end
 end
-
+ 
 function obj = calcMotion(obj,n,dt)
     for i = 1:n
         obj(i,5:7) = calcVelocity(i);
@@ -28,9 +27,9 @@ function obj = calcMotion(obj,n,dt)
     
     function velocity = calcVelocity(j)
         G = 1;
+        velocity = obj(j,5:7);
         for k = 1:n
             if k ~= j
-                velocity = obj(j,5:7);
                 distance = obj(k,2:4) - obj(j,2:4);
                 velocity = velocity + G*dt*distance*obj(k,1)/(norm(distance)^3);
             end
